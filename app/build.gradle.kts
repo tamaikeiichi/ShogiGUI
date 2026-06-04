16d01fb8c4f79fd20bf63cffc6a993d49dda3aa2
tamai <kpiyota@yahoo.co.jp>
2026-06-04 19:30:45 +0900

    fix graph remaining

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.tksoft.shogigui"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.9"
+        versionCode = 3
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
