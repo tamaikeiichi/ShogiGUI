262f18843543eb4084aa49d50a07b1c78950c0b4
tamaikeiichi <kpiyota@yahoo.co.jp>
2026-06-23 14:11:41 +0900

    minor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.tksoft.shogigui"
         minSdk = 24
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.461"
+        versionCode = 17
+        versionName = "1.47"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
