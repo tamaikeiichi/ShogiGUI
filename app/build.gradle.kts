52bf3314593d3507428b34fe854daf283ac28de7
tamaikeiichi <kpiyota@yahoo.co.jp>
2026-08-31 07:33:08 +0900

    fix sequence of next move and engine freeze problem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.tksoft.shogigui"
         minSdk = 24
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.50"
+        versionCode = 21
+        versionName = "1.51"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
